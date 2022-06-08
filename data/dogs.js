5cd87a8497191cd5ca87e6f33d7894f7ba12a622
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-08 12:08:03 -0700

    playing around with the dog stuff to see if postgres is working as intended

---
+++ data/dogs.js
@@ -0,0 +1,8 @@
+const dogs = [
+  { id: '1', name: 'Benny', age: 7 },
+  { id: '2', name: 'Ruby', age: 12 },
+  { id: '3', name: 'Finley', age: 4 },
+  { id: '4', name: 'Jeep', age: 4 },
+];
+
+module.exports = { dogs };
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists dogs;
+
+CREATE table dogs (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  age INT NOT NULL
+);
+
+INSERT INTO dogs (name, age) VALUES 
+('Benny', 7),
+('Ruby', 12),
+('Finley', 4),
+('Jeep', 4);
