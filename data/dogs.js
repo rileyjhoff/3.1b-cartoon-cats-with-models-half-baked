86eee9baafb7151dfe6d3d0283f0eec5915e5219
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-08 12:37:08 -0700

    remove dog files now that everything is working with cats

--- data/dogs.js
+++
@@ -1,8 +0,0 @@
-const dogs = [
-  { id: '1', name: 'Benny', age: 7 },
-  { id: '2', name: 'Ruby', age: 12 },
-  { id: '3', name: 'Finley', age: 4 },
-  { id: '4', name: 'Jeep', age: 4 },
-];
-
-module.exports = { dogs };
